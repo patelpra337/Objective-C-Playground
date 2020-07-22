64f713cceeac17e8e4ff5e50030c04c28287c230
Pravin Patel <patelpra337@gmail.com>
2020-07-21 20:36:10 -0500

    Initial Commit

---
+++ Objective-C Playground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C Playground
+//
+//  Created by patelpra on 7/21/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
