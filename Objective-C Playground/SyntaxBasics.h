a7206b085460985937c52cc5c1e3fa7a39058e31
Pravin Patel <patelpra337@gmail.com>
2020-07-21 20:58:25 -0500

    Create new Objective-C project; added custom Objective-C class

---
+++ Objective-C Playground/SyntaxBasics.h
@@ -0,0 +1,17 @@
+//
+//  SyntaxBasics.h
+//  Objective-C Playground
+//
+//  Created by patelpra on 7/21/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SyntaxBasics : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C Playground/SyntaxBasics.m
@@ -0,0 +1,13 @@
+//
+//  SyntaxBasics.m
+//  Objective-C Playground
+//
+//  Created by patelpra on 7/21/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+#import "SyntaxBasics.h"
+
+@implementation SyntaxBasics
+
+@end
